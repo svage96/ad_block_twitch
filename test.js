ce50a4531a9cddd3a7ecebaeb2f4bc39e25dcfeb
svage96 <svage96@users.noreply.github.com>
2020-11-06 21:46:43 -0500

    Update test.js

--- test.js
+++ test.js
@@ -9,21 +9,11 @@ window.fetch = (url, init, ...args) => {
 			return origFetch(url, init, ...args).then(function(res){response = res; return res.json()})
 						     .then(function(bod) {
  bod.token = bod.token.replace("\"hide_ads\":false", "\"hide_ads\":true").replace("\"server_ads\":true", "\"server_ads\":false").replace("\"show_ads\":true", "\"show_ads\":false");
-return 
-	new Response(bod, {
+return new Response(bod, {
 	    status: response.status,
 	    statusText: response.statusText,
 	    headers: response.headers
-	  }) ;
-
-  
-				
-				
-});
-			
-			
-			
-		} 
-	}
+	  });});		
+} }
 	return origFetch(url, init, ...args);
 };
