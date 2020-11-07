5e496620a69909a907ef0bd4dae65a5bfac6054a
svage96 <svage96@users.noreply.github.com>
2020-11-06 22:23:45 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,20 @@
 /// twitch-videoad.js
 const origFetch = window.fetch;
 window.fetch = (url, init, ...args) => {
-	if (typeof url === "string") {
-		if (url.includes("/access_token")) {
+	if (typeof url === "string" && url.includes("/access_token")) {
 			var response;
-			return origFetch(url, init, ...args).then(function(res){response = res; return res.json()})
-						     .then(function(bod) {
- bod.token = bod.token.replace("\"hide_ads\":false", "\"hide_ads\":true").replace("\"server_ads\":true", "\"server_ads\":false").replace("\"show_ads\":true", "\"show_ads\":false");
-return new Response(JSON.stringify(bod), {
-	    status: response.status,
-	    statusText: response.statusText,
-	    headers: response.headers
-	  });});		
-} }
+			return origFetch(url, init, ...args)
+            .then(function(res){response = res; return res.json()})
+			.then(function(new_body) {
+                new_body.token = new_body.token.replace("\"hide_ads\":false", "\"hide_ads\":true")
+                                               .replace("\"server_ads\":true", "\"server_ads\":false")
+                                               .replace("\"show_ads\":true", "\"show_ads\":false");
+                return new Response(JSON.stringify(new_body), {
+                                       status: response.status,
+                                       statusText: response.statusText,
+                                       headers: response.headers });
+            });		
+    }
+    
 	return origFetch(url, init, ...args);
 };
