6c0a3ccda72c46fa2933418db5bcad35725036ce
svage96 <svage96@users.noreply.github.com>
2020-11-06 22:01:17 -0500

    Update test.js

--- test.js
+++ test.js
@@ -3,13 +3,11 @@ const origFetch = window.fetch;
 window.fetch = (url, init, ...args) => {
 	if (typeof url === "string") {
 		if (url.includes("/access_token")) {
-          console.log('bruv');
 			var response;
-			var body;
 			return origFetch(url, init, ...args).then(function(res){response = res; return res.json()})
 						     .then(function(bod) {
  bod.token = bod.token.replace("\"hide_ads\":false", "\"hide_ads\":true").replace("\"server_ads\":true", "\"server_ads\":false").replace("\"show_ads\":true", "\"show_ads\":false");
-return new Response(bod, {
+return new Response(JSON.stringify(bod), {
 	    status: response.status,
 	    statusText: response.statusText,
 	    headers: response.headers
