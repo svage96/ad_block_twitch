d10f494c2bbc998ff5b0c0b1e648646e828cd7db
svage96 <svage96@users.noreply.github.com>
2020-11-06 21:16:36 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,23 @@
+const origFetch = window.fetch;
+window.fetch = (url, init, ...args) => {
+	if (typeof url === "string") {
+		if (url.includes("/access_token")) {
+          console.log('bruv');
+			var response;
+			var body;
+			origFetch(url, init, ...args).then(function(res){response = res; return res.json()})
+						     .then(function(bod) {
+ bod.token = bod.token.replace("\"hide_ads\":false", "\"hide_ads\":true").replace("\"server_ads\":true", "\"server_ads\":false").replace("\"show_ads\":true", "\"show_ads\":false");
+return new Response("ypypypyp", {
+    status: response.status,
+    statusText: response.statusText,
+    headers: response.headers
+  }) 
+});
+			
+			
+			
+		} 
+	}
+	return origFetch(url, init, ...args);
+};
